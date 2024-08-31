cf33cc5e5aad4fe54e22f12641b011851683554f
Angella-Mulikatete <mulikateteangella@gmail.com>
2024-08-31 17:25:26 +0300

    updated withdraw function

--- contracts/stakeERC.sol
+++ contracts/stakeERC.sol
@@ -10,35 +10,38 @@ contract StakeERC20{
 
     address public tokenAddress;
     address public owner;
-    uint256 public stakingDuration; //The duration for which tokens must be staked to receive full rewards
+   
 
-    struct Stake {
-        uint256 amount;
-        uint256 depositTime;
-        uint256 rewardClaimed;
-    }
+    uint256 public initialTimestamp;//deposit
+    bool public timestampSet;
+    uint256 public stakePeriod;
+     
 
    
-    constructor(address _tokenAddress, uint256 _stakingDuration) {
+    constructor(address _tokenAddress) {
         tokenAddress = _tokenAddress;
-        // rewardRate = _rewardRate;
-        stakingDuration = _stakingDuration * 60;
+        require(tokenAddress != address(0),"Invalid address");
         owner = msg.sender;
     }
 
     address[] public stakeHolders;
    
-    mapping(address => Stake) public stakes;
+    // mapping(address => Stake) public stakes;
     mapping(address => uint256) private stakeholderIndex;
     mapping(address => bool) private isStakeholder;
     mapping(address => uint) balances;
     mapping(address => uint) timeOfDeposit;
 
-    event stakedSuccessfully(uint256 indexed amount, uint256 indexed depositTime );
+    event stakedSuccessfully(address indexed addressFrom, uint256 indexed amount );
 
     modifier checkAddress(address _address){
          require(_address != address(0), "Address zero detected");
          _;
+    }
+
+    modifier onlyOwner(){
+        require(owner == msg.sender, "Only owner");
+        _;
     }
 
     //checking if an address is a stakeholder
@@ -74,93 +77,60 @@ contract StakeERC20{
         return balances[msg.sender];
     }
 
-    function deposit(uint _amount) external {
-        require(msg.sender != address(0), "address zero detected");
-        require(_amount > 0, "cant deposit zero amount");
-
-        uint256 stakeHolderBalance = IERC20(tokenAddress).balanceOf(msg.sender);
-        require(stakeHolderBalance >= _amount, "INSUFFICIENT FUNDS");
-
-        IERC20(tokenAddress).transferFrom(msg.sender, address(this), _amount); 
-        balances[msg.sender] += _amount;
-    }
 
     //stake your tokens
     function stakeToken(uint256 _amount) external {
         require(_amount > 0, "stake higher than zero");
         require(msg.sender != address(0), "address zero detected");
-        if(isStakeHolder(msg.sender)){
-            addStakeHolder(msg.sender);
-        }
 
         uint stakeHolderBalance = IERC20(tokenAddress).balanceOf(msg.sender);
         require(stakeHolderBalance > 0, "INSUFFICIENT FUNDS");
 
-        Stake memory stakeHolderStake = stakes[msg.sender];
-
-        timeOfDeposit[msg.sender] = block.timestamp;
+        IERC20(tokenAddress).safeTransferFrom(msg.sender, address(this), _amount); 
         balances[msg.sender] += _amount;
 
-        stakeHolderStake.amount += _amount;
-        stakeHolderStake.depositTime = block.timestamp;
-
-        emit stakedSuccessfully( stakeHolderStake.amount, stakeHolderStake.depositTime);
+        emit stakedSuccessfully( msg.sender, _amount);
 
     }
 
-    function calculateReward(address _staker) public  view returns(uint256){
-        Stake memory stakeHolderStake = stakes[_staker];
+    function calculateReward( uint256 _amount) private  view returns(uint256){
+        uint256 stakingTime = block.timestamp - timeOfDeposit[msg.sender];
 
-        uint256 stakingTime = block.timestamp - stakeHolderStake.depositTime;
-
-        if(stakingTime > stakingDuration){
-            stakingTime = stakingDuration;
+        if(stakingTime > stakePeriod){
+            return 0;
         }
 
-        uint256 rewardRate = (stakeHolderStake.amount)/(stakingDuration);
-
-        uint reward = (stakeHolderStake.amount * rewardRate * stakingTime) / (stakingDuration * 100);
+        uint256 rewardRate = (_amount)/(stakePeriod);
+        uint reward = (_amount * rewardRate * stakingTime) / (stakePeriod * 100);
         return reward;
 
     }
 
-    function depositTime() public view returns(uint256){
-        Stake memory sstakes = stakes[msg.sender];
-        return (sstakes.depositTime);
-    }
-
     function withdrawTime() external view returns(uint256){
-        Stake memory stakeHolderStake = stakes[msg.sender];
 
-        if((stakeHolderStake.depositTime *60) > stakingDuration){
+        if(( timeOfDeposit[msg.sender] *60) > stakePeriod){
             return 0;
         }else{
-            return (block.timestamp - (stakeHolderStake.depositTime *60));
+            return (block.timestamp - (timeOfDeposit[msg.sender] * 60));
         }
     }
 
     function withdraw(uint256 _amount) external payable{
         require(_amount > 0, "Cant withdraw zero tokens");
-        Stake memory stakeHolderStake = stakes[msg.sender];
+        require(balances[msg.sender] >= _amount, "INSUFFICIENT FUNDS");
 
-        stakeHolderStake.rewardClaimed = calculateReward(msg.sender);
-        uint256 reward = stakeHolderStake.rewardClaimed;
-        uint256 totalAmount = stakeHolderStake.amount += reward;
+        uint rewardClaimed = calculateReward(_amount);
+        uint256 totalAmount = _amount += rewardClaimed;
         
         IERC20(tokenAddress).safeTransfer(msg.sender, totalAmount);
 
     }
 
     function updateStakingDuration(uint _newStakingDuration) internal {
-        stakingDuration = _newStakingDuration;
+        stakePeriod = _newStakingDuration;
     }
 
-    function getStakes(address Staker) external view returns(uint256 _amount, uint256 _depositTime) {
-        Stake memory _stakes = stakes[Staker];
-        return(_stakes.amount, _stakes.depositTime);
-    }
-
-    function getAllStakesInContract() internal view returns(uint256){
+    function getAllStakesInContract() internal view onlyOwner returns(uint256){
         return IERC20(tokenAddress).balanceOf(address(this));
     }
 
