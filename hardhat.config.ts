61dd36dbaa77699031131cb00abea9c5d6541402
Angella-Mulikatete <mulikateteangella@gmail.com>
2024-10-04 02:42:35 +0300

    added deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,41 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    // for testnet
+    "lisk-sepolia": {
+      url: process.env.LISK_RPC_URL!,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY!],
+      gasPrice: 1000000000,
+    },
+  },
+  etherscan: {
+    // Use "123" as a placeholder, because Blockscout doesn't need a real API key, and Hardhat will complain if this property isn't set.
+    apiKey: {
+      "lisk-sepolia": "123",
+    },
+    customChains: [
+      {
+        network: "lisk-sepolia",
+        chainId: 4202,
+        urls: {
+          apiURL: "https://sepolia-blockscout.lisk.com/api",
+          browserURL: "https://sepolia-blockscout.lisk.com/",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: false,
+  },
 };
 
 export default config;
+
+// myToken#myToken - 0xDB74B05CCB67Be88930f90eec88610B12865bb9a
+// StakeERC20Module#StakeERC20 - 0xC6e078f3F1d01eFfdD715afE08e5674B5CF3e722
+// StakeEthModule#StakeEth - 0xe71d36bbD21E135aB892B27645B89d4FC9179086
--- ignition/modules/stakeErc.ts
+++ ignition/modules/stakeErc.ts
@@ -1,14 +1,13 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const tokenAddress = " ";
+const tokenAddress = "0xDB74B05CCB67Be88930f90eec88610B12865bb9a";
 const DEFAULT_WITHDRAW_DEADLINE = 60 * 60 * 24 * 30;
 
 const StakeERC20Module = buildModule("StakeERC20Module", (m) => {
   const withdrawDeadline = m.getParameter("withdrawDeadline", DEFAULT_WITHDRAW_DEADLINE);
 
-    const stake = m.contract("StakeERC20", [tokenAddress, withdrawDeadline]);
-
-    return { stake };
+  const stake = m.contract("StakeERC20", [tokenAddress, withdrawDeadline]);
+  return { stake };
 });
 
 export default StakeERC20Module;
